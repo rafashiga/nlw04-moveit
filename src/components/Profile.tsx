981dcd5d6be6e9765023adf537cf44019a4d4729
Rafael <rafa.shiga@gmail.com>
2021-02-24 21:09:28 -0300

    feat: start challenge

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,7 +3,7 @@ import styles from '../styles/components/Profile.module.css';
 export const Profile = () => {
 	return (
 		<div className={styles.profileContainer}>
-			<img src='https://github.com/diego3g.png' alt='Usuário' />
+			<img src='https://github.com/rafashiga.png' alt='Usuário' />
 			<div>
 				<strong>Nome do usuário</strong>
 				<p>
